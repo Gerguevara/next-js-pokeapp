c10fdd7ec47d189d6c840a51387748ae7a2a8807
Gerardo <gerardoguevara64@gmail.com>
2023-05-28 13:51:22 -0600

    getStatic Props explanation

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -8,7 +8,6 @@ interface Props {
   children: ReactNode;
 }
 
-
 const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
 
@@ -22,7 +21,6 @@ export const Layout: FC<Props> = ({ children, title }) => {
             <meta name="author" content="Fernando Herrera" />
             <meta name="description" content={`Información sobre el pokémon ${ title }`} />
             <meta name="keywords" content={ `${ title }, pokemon, pokedex`} />
-
             <meta property="og:title" content={`Información sobre ${ title }`} />
             <meta property="og:description" content={`Esta es la página sobre ${ title }`} />
             <meta property="og:image" content={`${ origin }/img/banner.png`} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,36 @@ import type { NextPage } from "next";
 
 import { Button } from "@nextui-org/react";
 import { Layout } from "../components/layouts";
+import { useEffect } from "react";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({name}: any) => {
+
+  useEffect(() => {
+    console.log(name);
+  })
   return (
     <>
-      <Layout>Click me</Layout>
+      <Layout>
+        <Button>Click me!</Button>
+      </Layout>
     </>
   );
 };
+
+//las props que se pasen aca son utilizadas  por el componente
+// en la linea 6
+
+// get static props solo se ejecuta en el lado del servidor y solo el build time
+
+
+// get static props solo se puede utilizar en pages
+export const getServerSideProps =  (context: any) => {
+  return {
+    props: {
+      name: 'Gerardo'
+    },
+  };
+};
+
 
 export default Home;
